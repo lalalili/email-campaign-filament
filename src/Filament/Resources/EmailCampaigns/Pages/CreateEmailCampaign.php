3fa9148561f7fbcb5631cf4177ac20688f2148f9
James Liang <lalalili@gmail.com>
2026-07-17 15:25:06 +0800

    fix: 建立 Email 活動後返回清單

--- src/Filament/Resources/EmailCampaigns/Pages/CreateEmailCampaign.php
+++ src/Filament/Resources/EmailCampaigns/Pages/CreateEmailCampaign.php
@@ -16,17 +16,12 @@ class CreateEmailCampaign extends CreateRecord
 {
     protected static string $resource = EmailCampaignResource::class;
 
-    protected function getRedirectUrl(): string
-    {
-        return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
-    }
-
     protected function getCreatedNotification(): ?Notification
     {
         return Notification::make()
             ->success()
             ->title('活動已建立')
-            ->body('可在下方檢視收件人快照與寄送紀錄。');
+            ->body('可從活動清單檢視收件人快照與寄送紀錄。');
     }
 
     protected function afterCreate(): void
